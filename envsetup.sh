5ac1b339f05d5cde2eb2aeac97870e253d0bab83
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-05-12 16:49:03 +0200

    DK_RELEASE_TYPE: Only allow type unofficial, alpha or beta

--- envsetup.sh
+++ envsetup.sh
@@ -515,6 +515,8 @@ function breakfast()
     local type=$2
     if [ -z "$type" ]; then
         type="UNOFFICIAL"
+    elif [ "$type" != "ALPHA" ]; then
+        type="BETA"
     fi
     export DK_RELEASE_TYPE=$type
     DK_DEVICES_ONLY="true"
